537b93a4eef5a4eb6a7a446cfec8f9a692b310c9
raven <laingraven.java@gmail.com>
2024-03-15 21:18:16 +0700

    update

--- src/test/java/raven/swing/blur/demo/PanelItem.java
+++ src/test/java/raven/swing/blur/demo/PanelItem.java
@@ -1,18 +1,47 @@
 package raven.swing.blur.demo;
 
+import com.formdev.flatlaf.FlatClientProperties;
 import net.miginfocom.swing.MigLayout;
 import raven.swing.blur.BlurChild;
 
 import javax.swing.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
 
 public class PanelItem extends BlurChild {
+
+    private int panelX;
+    private int panelY;
+
 
     public PanelItem() {
         init();
     }
 
     private void init() {
-        setLayout(new MigLayout());
-        add(new JButton("Hello"));
+        setLayout(new MigLayout("al center center"));
+        JLabel label = new JLabel("Move me");
+        label.putClientProperty(FlatClientProperties.STYLE, "" +
+                "foreground:#FFFFFF;" +
+                "font:bold +5");
+        add(label);
+
+        MouseAdapter mouseAdapter = new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                panelX = e.getX();
+                panelY = e.getY();
+            }
+
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                int deltaX = e.getX() - panelX;
+                int deltaY = e.getY() - panelY;
+                setLocation(getX() + deltaX, getY() + deltaY);
+            }
+        };
+        addMouseListener(mouseAdapter);
+        addMouseMotionListener(mouseAdapter);
     }
 }
